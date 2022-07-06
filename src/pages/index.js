2dd3ef3f311383986b7e381cbf280265963b34ac
Erdem Aydoğdu <erdemaydogdu@outlook.com>
2022-07-06 13:13:25 +0300

    colorize background

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,23 @@
 import * as React from "react"
 import linkedinImage from "../images/linkedin.png"
 import twitterImage from "../images/twitter.png"
-
+import '../styles/style.css'
 
 // styles
 const pageStyles = {
-  color: "#232129",
+  color: "#DDDDDD",
   padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  fontFamily: "-apple-system, Roboto, sans-serif, serif"
 }
 const headingStyles = {
   marginTop: 0,
-  marginBottom: 64,
+  marginBottom: 64
 }
 const headingAccentStyles = {
-  color: "#663399",
+  color: "#CCCCCC",
   fontSize: "1.50rem"
 }
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
 
-}
 const listItemStyles = {
   fontWeight: 300,
   fontSize: 24,
@@ -44,36 +30,6 @@ const linkStyle = {
   fontWeight: "bold",
   fontSize: 16,
   verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
 }
 
 const imageStyle = {
